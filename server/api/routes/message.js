61eb3d47d6ed33d5f6db35fcb0dc755eae450460
Petre Diana <petre.diana98@yahoo.ro>
2020-04-13 15:45:38 +0300

    added route to get all the messages for a conversation

--- server/api/routes/message.js
+++ server/api/routes/message.js
@@ -46,6 +46,24 @@ router.get('/:userId', async (req, res, next) => {
     }
 });
 
+// get all messages for a conversation
+router.post('/specific-conversation', async(req, res, next) => {
+    try {
+        const allMessages = await Message.find().select('_id message info saidBy conversationId');;
+        const conversationId = req.body.conversationId;
+        const conversationMessages = allMessages.filter(message => {
+            if (String(message.conversationId) === String(conversationId)) {
+                return message;
+            }
+        });
+
+        res.status(200).json(conversationMessages);
+    } catch(err) {
+        console.warn(err);
+        next(err);
+    }
+});
+
 router.post('/', (req, res, next) => {
     try {
         const dbMessage = new Message({
